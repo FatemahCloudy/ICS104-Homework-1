a6efcd91a041e5e1597985326061c701e94eb249
FatemahCloudy <165394686+FatemahCloudy@users.noreply.github.com>
2024-04-07 23:49:38 +0300

    Update HW1Q2.py

--- HW1Q2.py
+++ HW1Q2.py
@@ -1,9 +1,3 @@
-# Your code for Question 2 in this cell
-# After you finish writing your code and check that it is working correctly, 
-# uncomment the first line and run your code again so that it genrates HW1Q2.py file
-
-##
-
 #get two numbers from user and chek if they are positive and different
 
 num1 = int(input("Enter a positive integer: "))
