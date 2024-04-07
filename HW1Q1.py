3160d21ff081d23919d2b9b9c432376bdb36fcf8
FatemahCloudy <165394686+FatemahCloudy@users.noreply.github.com>
2024-04-07 23:49:13 +0300

    Update HW1Q1.py

--- HW1Q1.py
+++ HW1Q1.py
@@ -1,10 +1,3 @@
-# Your code for Question 1 in this cell
-# After you finish writing your code and check that it is working correctly, 
-# uncomment the first line (#%%writefile HW1Q1.py) and run your code again so that 
-#it genrates HW1Q1.py file
-
-##
-
 #Ask about colour
 colour = input("What is the colour of the cylender?")
 colour = colour.lower()
